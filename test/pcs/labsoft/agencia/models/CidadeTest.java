722f5cadf2506ffa55dce7cc1968331cb9da91e9
Leonardo Iacovini <leonardo.iacovini@nubank.com.br>
2016-11-04 15:33:23 -0200

    Create test and res dirs

---
+++ test/pcs/labsoft/agencia/models/CidadeTest.java
@@ -0,0 +1,22 @@
+package pcs.labsoft.agencia.models;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by leoiacovini on 11/4/16.
+ */
+public class CidadeTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
